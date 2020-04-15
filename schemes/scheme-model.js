fbd353f80c268f02ee927e2f4174b4b84f51b282
MrR3set <bairon.pazrobles@gmail.com>
2020-04-15 15:37:52 -0700

    initial

---
+++ schemes/scheme-model.js
@@ -0,0 +1,31 @@
+const db = require("../data/db-config");
+module.exports = {
+    find,
+    findById,
+    findSteps,
+    add,
+    remove
+}
+
+function find(){
+    return db("schemes");
+}
+
+function findById(id){
+    return db("schemes").where({"id":id});
+}
+
+function findSteps(id){
+    
+}
+function add(data){
+    return db("schemes").insert(data);
+}
+
+function update(data,id){
+    return db("schemes").where({"id":id}).update(data);
+}
+
+function remove(id){
+    return db("schemes").where({"id":id}).del();
+}
